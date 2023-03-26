60edf231a4830debb61ba3289a95925bcfb67522
IagoLast <iago.lastra@gmail.com>
2023-03-26 23:21:24 +0200

    feat: Header

---
+++ src/app/head.tsx
@@ -0,0 +1,13 @@
+export default function Head() {
+  return (
+    <head>
+      <title>Robot legal</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta
+        name="description"
+        content="Resuelve tus dudas legales al instante utilizando inteligencia artificial."
+      />
+    </head>
+  );
+}
